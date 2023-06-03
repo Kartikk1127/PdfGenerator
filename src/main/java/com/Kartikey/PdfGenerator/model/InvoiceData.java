39ada2ce42d1aacd5b347d67a21ecf009f09c345
Kartikey <kartikey.srivastava1271@gmail.com>
2023-06-04 00:21:23 +0530

    model created

---
+++ src/main/java/com/Kartikey/PdfGenerator/model/InvoiceData.java
@@ -0,0 +1,20 @@
+package com.Kartikey.PdfGenerator.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class InvoiceData {
+    private String seller;
+    private String sellerGstin;
+    private String sellerAddress;
+    private String buyer;
+    private String buyerGstin;
+    private String buyerAddress;
+    private List<InvoiceItem> items;
+}
